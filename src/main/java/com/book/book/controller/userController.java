21d456765f9f6a71907d8a4030402801ada4bf11
汤光会 <3474941138@qq.com>
2024-12-23 16:31:00 +0800

    tth提交啦

---
+++ src/main/java/com/book/book/controller/userController.java
@@ -0,0 +1,14 @@
+package com.book.book.controller;
+
+import com.book.book.utils.ResponseUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class userController {
+    private ResponseUtils list(){
+        System.out.println("list");
+        return null;
+    }
+}
