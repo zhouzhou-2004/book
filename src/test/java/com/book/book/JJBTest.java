1e0c5528e409396a149d7760d9bc1649a5b553f1
dx12222 <3059057392@qq.com>
2024-12-23 09:16:56 +0800

    dx 删除

--- src/test/java/com/book/book/JJBTest.java
+++
@@ -1,7 +0,0 @@
-package com.book.book;
-
-public class JJBTest {
-    public static void main(String[] args) {
-        System.out.println("JJB来喽");
-    }
-}
--- src/test/java/com/book/book/TTHTest.java
+++
@@ -1,7 +0,0 @@
-package com.book.book;
-
-public class TTHTest {
-    public static void main(String[] args) {
-        System.out.println("tgh来喽");
-    }
-}
