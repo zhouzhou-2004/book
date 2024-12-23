c98f877a1408a8cafc0b40875f613feb5d807af0
dx12222 <3059057392@qq.com>
2024-12-23 17:26:31 +0800

    dx 前端页面修改

--- src/main/java/com/book/book/mapper/userMapper.java
+++ src/main/java/com/book/book/mapper/userMapper.java
@@ -1,11 +1,17 @@
 package com.book.book.mapper;
 
-import org.apache.catalina.User;
+import com.book.book.model.pojo.Users;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface userMapper {
+
+    //dx   登录
+    @Select("select * from users where username = #{username} and password = #{password} and is_delete = 0")
+    Users login(Users tAdmin);
+
+
 //    //用户管理
 //    @Select("select * from user where is_admin = 0")
 //    List<User> getAllUser();
