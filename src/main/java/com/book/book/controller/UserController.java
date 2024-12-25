ad0227c28bdad3dd23fdb0dcf2581e8e65700b16
汤光会 <3474941138@qq.com>
2024-12-25 20:40:08 +0800

    tth提交

--- src/main/java/com/book/book/controller/UserController.java
+++ src/main/java/com/book/book/controller/UserController.java
@@ -5,6 +5,8 @@ import com.book.book.model.vo.UserVO;
 import com.book.book.service.UserService;
 import com.book.book.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,24 +14,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
-    @RequestMapping("/list")
-    private ResponseUtils list(){
+    @GetMapping("/list")
+    public ResponseUtils<List<UserVO>> list() {
         try {
             List<UserVO> allUser = userService.getAllUser();
-//            System.out.println("查询结果："+allUser);
-            if (allUser != null){
-                //查询成功
-                return new ResponseUtils(200,"查询成功",allUser);
-            }else {
-                //查询为空(失败)
-                return new ResponseUtils(500,"查询失败");
+            System.out.println("查询结果：" + allUser);
+            if (allUser != null) {
+                return new ResponseUtils<>(200, "查询成功", allUser);
+            } else {
+                return new ResponseUtils<>(500, "查询失败");
             }
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseUtils(400,"查询异常");
+            return new ResponseUtils<>(400, "查询异常");
         }
     }
 }
