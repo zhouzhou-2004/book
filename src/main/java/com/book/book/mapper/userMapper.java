8403660bc80e63a4b6992feed8e0fca5139c03e9
dx12222 <3059057392@qq.com>
2024-12-27 10:10:01 +0800

    dx 注册登录大致完善

--- src/main/java/com/book/book/mapper/userMapper.java
+++ src/main/java/com/book/book/mapper/userMapper.java
@@ -1,20 +1,12 @@
 package com.book.book.mapper;
 
-import com.book.book.model.pojo.Users;
-import org.apache.ibatis.annotations.Mapper;
+import com.book.book.model.vo.UserVO;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-@Mapper
-public interface userMapper {
-
-    //dx   登录
-    @Select("select * from users where username = #{username} and password = #{password}")
-    Users login(Users users);
-
-
-//    //用户管理
-//    @Select("select * from user where is_admin = 0")
-//    List<User> getAllUser();
+public interface UserMapper {
+    //用户管理
+    @Select("select * from users where is_admin = 0")
+    List<UserVO> getAllUser();
 }
