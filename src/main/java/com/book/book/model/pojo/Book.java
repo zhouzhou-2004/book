2218f9383ea311fe08ced1ae613a0a07db627d62
zsy <3315847862@qq.com>
2024-12-30 16:15:37 +0800

    搜索图书页面功能（Book添加get和set）

--- src/main/java/com/book/book/model/pojo/Book.java
+++ src/main/java/com/book/book/model/pojo/Book.java
@@ -21,5 +21,83 @@ public class Book {
   private int size;
   private String type;
 
+  public int getPages() {
+    return pages;
+  }
 
+  public void setPages(int pages) {
+    this.pages = pages;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String getPublish() {
+    return publish;
+  }
+
+  public void setPublish(String publish) {
+    this.publish = publish;
+  }
+
+  public String getPublishTime() {
+    return publishTime;
+  }
+
+  public void setPublishTime(String publishTime) {
+    this.publishTime = publishTime;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
